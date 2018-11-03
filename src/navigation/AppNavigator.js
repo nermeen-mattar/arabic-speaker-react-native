29b56c601662fef96d5a6f74498ef78c2d34f6c5
nermeen-mattar <nermeenmattar5d@gmail.com>
2018-11-03 16:14:56 +0200

    Adding new category to switch navigator (temp)

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,9 +1,13 @@
 import { createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import NewCategoryScreen from '../screens/NewCategoryScreen';
 
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
+  NewCategory: {
+    screen: NewCategoryScreen
+  },
 });
