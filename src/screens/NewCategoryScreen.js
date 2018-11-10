410015f83cf0e632f65ef739969014a2fcb04190
nermeen-mattar <nermeenmattar5d@gmail.com>
2018-11-10 22:01:41 +0200

    Displaying camera roll

--- src/screens/NewCategoryScreen.js
+++ src/screens/NewCategoryScreen.js
@@ -14,7 +14,8 @@ import {
 import { MonoText } from '../components/StyledText';
 import FormHeader from '../components/FormHeader';
 import Colors from '../constants/Colors';
-// import { Icon } from 'expo';
+import ImagePicker from 'react-native-image-picker';
+
 import PhotoUpload from 'react-native-photo-upload'
 import { Storage } from '../classes/storage';
 
@@ -28,6 +29,42 @@ export default class NewCategoryScreen extends React.Component {
           categoryName: ''
         };
         props.navigation.addListener('willFocus', this.load)
+
+
+
+        // More info on all the options is below in the API Reference... just some common use cases shown here
+const options = {
+  title: 'Select Avatar',
+  customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
+        /**
+ * The first arg is the options object for customization (it can also be null or omitted for default options),
+ * The second arg is the callback which sends object: response (more info in the API Reference)
+ */
+ImagePicker.showImagePicker(options, (response) => {
+  console.log('Response = ', response);
+
+  if (response.didCancel) {
+    console.log('User cancelled image picker');
+  } else if (response.error) {
+    console.log('ImagePicker Error: ', response.error);
+  } else if (response.customButton) {
+    console.log('User tapped custom button: ', response.customButton);
+  } else {
+    const source = { uri: response.uri };
+
+    // You can also display the image using data:
+    // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+    this.setState({
+      avatarSource: source,
+    });
+  }
+});
       }
 
       load = () => {
@@ -38,6 +75,8 @@ export default class NewCategoryScreen extends React.Component {
       static navigationOptions = {
         header: null
       };
+      
+
       
       render() {
     return (
