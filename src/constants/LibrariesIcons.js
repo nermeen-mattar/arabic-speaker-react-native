21eb1ac61fb7384e5ad1ae6ff4381d7a01e39d6a
nermeen-mattar <nermeenmattar5d@gmail.com>
2019-01-01 18:28:12 +0200

    Adding more

--- src/constants/LibrariesIcons.js
+++ src/constants/LibrariesIcons.js
@@ -1,6 +1,5 @@
 export default {
-    'أيقونات الناس': [
-        {
+    'أيقونات الناس': [{
             label: 'بكاء',
             imgSrc: require('../../assets/images/libraries-icons/people/cry.jpg')
         },
@@ -125,22 +124,26 @@ export default {
             label: 'خروف',
             imgSrc: require('../../assets/images/libraries-icons/animals/cheep.gif')
         },
-        // {
-        //     label: 'ذبابة',
-        //     imgSrc:  require('../../assets/images/libraries-icons/animals/pencil.png')
-        // },
-        // {
-        //     label: 'دبور',
-        //     imgSrc:  require('../../assets/images/libraries-icons/animals/pencil.png')
-        // },
+        {
+            label: 'دجاجة',
+            imgSrc: require('../../assets/images/libraries-icons/animals/chicken.gif')
+        },
+        {
+            label: 'ذبابة',
+            imgSrc: require('../../assets/images/libraries-icons/animals/wasp.png')
+        },
+        {
+            label: 'دبور',
+            imgSrc: require('../../assets/images/libraries-icons/animals/mosquito.png')
+        },
         {
             label: 'صرصور',
             imgSrc: require('../../assets/images/libraries-icons/animals/cockroach.gif')
         },
-        // {
-        //     label: 'فراشة',
-        //     imgSrc:  require('../../assets/images/libraries-icons/animals/butterfly.gif')
-        // },
+        {
+            label: 'فراشة',
+            imgSrc: require('../../assets/images/libraries-icons/animals/butterfly.gif')
+        },
         {
             label: 'جمل',
             imgSrc: require('../../assets/images/libraries-icons/animals/camel.gif')
@@ -153,19 +156,18 @@ export default {
             label: 'سمكة',
             imgSrc: require('../../assets/images/libraries-icons/animals/fish.gif')
         },
-        // {
-        //     label: 'فقس بيضة',
-        //     imgSrc:  require('../../assets/images/libraries-icons/animals/pencil.png')
-        // },
-        // {
-        //     label: 'رائحة',
-        //     imgSrc:  require('../../assets/images/libraries-icons/animals/pencil.png')
-        // },
-        // {
-        //     label: 'رائحة نتنة',
-        //     imgSrc:  require('../../assets/images/libraries-icons/animals/pencil.png')
-        // }  
-
+        {
+            label: 'فقس بيضة',
+            imgSrc: require('../../assets/images/libraries-icons/animals/hatch.png')
+        },
+        {
+            label: 'رائحة',
+            imgSrc: require('../../assets/images/libraries-icons/animals/smell.png')
+        },
+        {
+            label: 'رائحة نتنة',
+            imgSrc: require('../../assets/images/libraries-icons/animals/stinky.png')
+        }
     ],
     'أيقونات المدارس': [{
             label: 'حاسبة',
@@ -226,158 +228,156 @@ export default {
             imgSrc: require('../../assets/images/libraries-icons/school/red.gif')
         }
     ],
-    'أيقونات الملابس': [
-        {
+    'أيقونات الملابس': [{
             label: 'تغيير الملابس',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/change-clothes.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/change-clothes.gif')
         },
         {
             label: 'فتح الزر',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/button.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/button.gif')
         },
         {
             label: 'اغلاق الزر',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/unbutton.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/unbutton.gif')
         },
         {
             label: 'ضيق جدآ',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/too-big.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/too-big.gif')
         },
         {
             label: 'كبير جدآ',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/too-small.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/too-small.gif')
         },
         {
             label: 'متسخ',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/dirty.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/dirty.gif')
         },
         {
             label: 'ارتداء الملابس',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/get-dressed.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/get-dressed.gif')
         },
         {
             label: 'فتح السحاب',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/zip.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/zip.gif')
         },
         {
             label: 'اغلاق السحاب',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/unzip.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/unzip.gif')
         },
         {
             label: 'بنطال مبلل',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/wet-pants.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/wet-pants.gif')
         },
         {
             label: 'ملابس داخلية',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/underwear.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/underwear.gif')
         },
         {
             label: 'شراب',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/socks.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/socks.gif')
         },
         {
             label: 'حذاء',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/shoes.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/shoes.gif')
         },
         {
             label: 'حذاء رياضي',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/running-shoes.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/running-shoes.gif')
         },
         {
             label: 'ملابس',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/clothes.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/clothes.gif')
         },
         {
             label: 'محفظة',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/wallet.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/wallet.gif')
         },
         {
             label: 'ساعة',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/watch.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/watch.gif')
         },
         {
             label: 'أقراط أذان',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/pierced-ears.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/pierced-ears.gif')
         },
         {
             label: 'نظارات',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/glasses.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/glasses.gif')
         },
         {
             label: 'منديل يد',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/handkerchief.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/handkerchief.gif')
         },
         {
             label: 'حقيبة ظهر',
-            imgSrc:  require('../../assets/images/libraries-icons/clothing/backpack.gif')
+            imgSrc: require('../../assets/images/libraries-icons/clothing/backpack.gif')
         }
     ],
-    'أيقونات الأماكن': [
-        {
+    'أيقونات الأماكن': [{
             label: 'مدرسة',
-            imgSrc:  require('../../assets/images/libraries-icons/places/school.gif')
-          },
-       
-          {
+            imgSrc: require('../../assets/images/libraries-icons/places/school.gif')
+        },
+
+        {
             label: 'منزل',
-            imgSrc:  require('../../assets/images/libraries-icons/places/home.gif')
-          },      
-        //   {
-        //     label: 'مسجد',
-        //     imgSrc:  require('../../assets/images/libraries-icons/places/mosque.gif')
-        //   },
-          {
+            imgSrc: require('../../assets/images/libraries-icons/places/home.gif')
+        },
+        {
+            label: 'مسجد',
+            imgSrc: require('../../assets/images/libraries-icons/places/mosque.gif')
+        },
+        {
             label: 'مستشفى',
-            imgSrc:  require('../../assets/images/libraries-icons/places/hospital.gif')
-          },
-          {
+            imgSrc: require('../../assets/images/libraries-icons/places/hospital.gif')
+        },
+        {
             label: 'مكتبة',
-            imgSrc:  require('../../assets/images/libraries-icons/places/library.gif')
-          },
-          {
+            imgSrc: require('../../assets/images/libraries-icons/places/library.gif')
+        },
+        {
             label: 'غرفة النوم',
-            imgSrc:  require('../../assets/images/libraries-icons/places/bedroom.gif')
-          },
-          {
+            imgSrc: require('../../assets/images/libraries-icons/places/bedroom.gif')
+        },
+        {
             label: 'حمام',
-            imgSrc:  require('../../assets/images/libraries-icons/places/bathroom.gif')
-          },
-          {
+            imgSrc: require('../../assets/images/libraries-icons/places/bathroom.gif')
+        },
+        {
             label: 'المطبخ',
-            imgSrc:  require('../../assets/images/libraries-icons/places/kitchen.gif')
-          },
+            imgSrc: require('../../assets/images/libraries-icons/places/kitchen.gif')
+        },
         //   {
         //     label: 'شمال',
         //     imgSrc:  require('../../assets/images/libraries-icons/places/north.gif')
         //   },
-          {
+        {
             label: 'جنوب',
-            imgSrc:  require('../../assets/images/libraries-icons/places/south.gif')
-          },
-          {
+            imgSrc: require('../../assets/images/libraries-icons/places/south.gif')
+        },
+        {
             label: 'شرق',
-            imgSrc:  require('../../assets/images/libraries-icons/places/east.gif')
-          },
-          {
+            imgSrc: require('../../assets/images/libraries-icons/places/east.gif')
+        },
+        {
             label: 'غرب',
-            imgSrc:  require('../../assets/images/libraries-icons/places/west.gif')
-          },
-          {
+            imgSrc: require('../../assets/images/libraries-icons/places/west.gif')
+        },
+        {
             label: 'الطابق العلوي',
-            imgSrc:  require('../../assets/images/libraries-icons/places/upstairs.gif')
-          },
-          {
+            imgSrc: require('../../assets/images/libraries-icons/places/upstairs.gif')
+        },
+        {
             label: 'الطابق السفلي',
-            imgSrc:  require('../../assets/images/libraries-icons/places/dowenstairs.gif')
-          },
-          {
+            imgSrc: require('../../assets/images/libraries-icons/places/dowenstairs.gif')
+        },
+        {
             label: 'مفتاح',
-            imgSrc:  require('../../assets/images/libraries-icons/places/key.gif')
-          },
-          {
+            imgSrc: require('../../assets/images/libraries-icons/places/key.gif')
+        },
+        {
             label: 'سيارة اسعاف',
-            imgSrc:  require('../../assets/images/libraries-icons/places/ambulance.gif')
-          },
+            imgSrc: require('../../assets/images/libraries-icons/places/ambulance.gif')
+        },
     ],
     'أيقونات النباتات': [
 
