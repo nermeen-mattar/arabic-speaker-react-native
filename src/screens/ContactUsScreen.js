37d69c2479b4f5e93bb86ab414e832c9d84451df
nermeen-mattar <nermeenmattar5d@gmail.com>
2019-02-03 15:35:11 +0200

    Improve contact us handling different cases

--- src/screens/ContactUsScreen.js
+++ src/screens/ContactUsScreen.js
@@ -1,112 +1,159 @@
-
-
-
-import React from 'react';
+import React from "react";
 import {
   StyleSheet,
   View,
   TextInput,
-  ScrollView,
+  Alert,
   TouchableOpacity,
-  Modal
-} from 'react-native';
+  Modal,
+  Linking
+} from "react-native";
 
-import { MonoText } from '../components/StyledText';
-import Colors from '../constants/Colors';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { TextToSpeach } from '../classes/TextToSpeach';
-import CustomHeader from '../components/CustomHeader';
-import commonStyles from '../styles/commonStyles';
-
+import { MonoText } from "../components/StyledText";
+import Colors from "../constants/Colors";
+import CustomHeader from "../components/CustomHeader";
+import commonStyles from "../styles/commonStyles";
 
 export default class ContactUsScreen extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-          title:[ "تواصل معنا"],
-
-          formFields: [
-            {
-              name: 'name',
-              label: 'الإسم',
-              type: 'text'
-          },
-          {
-            label: 'البريد الإلكتروني',
-            type: 'text',
-            name: 'email'
-
+  constructor(props) {
+    super();
+    this.state = {
+      title: ["تواصل معنا"],
+      // userMessage: {
+      //   type: 'success',
+      //   text: ''
+      // },
+      formFields: {
+        name: {
+          label: "الإسم",
+          type: "text"
         },
-        {
-          label: 'عنوان الرسالة',
-          type: 'text',
-          name: 'subject'
-        }, {
-          name: 'body',
-          label: 'نص السالة',
-          type: 'text',
+        email: {
+          label: "البريد الإلكتروني",
+          type: "text"
+        },
+        subject: {
+          label: "عنوان الرسالة",
+          type: "text"
+        },
+        body: {
+          label: "نص السالة",
+          type: "text",
           multiline: true,
-          style: {height: 121}
-        }]
-        };
+          style: { height: 121 }
+        }
       }
+    };
+  }
 
+  static navigationOptions = {
+    header: null
+  };
 
-      load = () => {
-          this.setState({
-          });
-       }
-      static navigationOptions = {
-        header: null
-      };
-      
-
-      
-      render() {
+  render() {
     return (
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={this.state.isVisible}
+      >
+        <CustomHeader
+          navigation={this.props.navigation}
+          title={this.state.title}
+          onBackClicked={() => {
+            this.props.onBackClicked();
+          }}
+        />
 
-      <Modal animationType="slide" transparent={false} visible={this.state.isVisible}>
-
-     <CustomHeader navigation = {this.props.navigation} title={this.state.title} onBackClicked= {() => {this.props.onBackClicked()}}/>
-
-
-    <View style= {[styles.container, commonStyles.flexCenter]} > 
-      <View>
+        <View style={[styles.container, commonStyles.flexCenter]}>
+          <View>
             {/* style={{flexDirection: 'row', justifyContent: 'center'}}>  */}
-    {
-            this.state.formFields.map((field, index) => {
-              return(
-                <TextInput  style={[styles.textInput, styles.inputStyle, field.style || {}]} onChangeText={(text) => this.onTextChanged(text, index)}
-                placeholder= {field.label}  multiline = {field.multiline}  />
-                // value={field.value}
-              )
-              
-              })}
-  
-     <TouchableOpacity style={[styles.button, styles.inputStyle]} onPress={ () => this.sendMessage()}  >
-          <MonoText style={styles.buttonText} >إرسال الرسالة</MonoText>
-        </TouchableOpacity>
-        </View>
+            {Object.keys(this.state.formFields).map(fieldName => {
+              return (
+                <TextInput
+                  style={[
+                    styles.textInput,
+                    styles.inputStyle,
+                    this.state.formFields[fieldName].style || {}
+                  ]}
+                  onChangeText={text => this.onTextChanged(text, fieldName)}
+                  placeholder={this.state.formFields[fieldName].label}
+                  multiline={this.state.formFields[fieldName].multiline}
+                  value ={this.state.formFields[fieldName].value}
+                />
+              );
+            })}
 
-    </View>
-                   
+            <TouchableOpacity
+              style={[styles.button, styles.inputStyle]}
+              onPress={() => this.sendMessage()}
+            >
+              <MonoText style={styles.buttonText}>إرسال الرسالة</MonoText>
+            </TouchableOpacity>
+          </View>
+
+          {/* <MonoText>
+      {this.state.userMessage}
+        </MonoText>
+                    */}
+        </View>
       </Modal>
     );
   }
 
   sendMessage() {
-
+    // const errorFields = [];
+    // this.state.formFields.forEach(field => {
+    //   if(!field.value) {
+    //     errorFields.push(field.label);
+    //   }
+    // });
+    // errorFields.length
+    const hasError = Object.keys(this.state.formFields).some(
+      fieldName => !this.state.formFields[fieldName].value
+    );
+    if (hasError) {
+      this.displayAlertMessage();
+      return;
+    } else {
+      body = 'اسم المرسل: '.concat(this.state.formFields.name.value).concat(' \n ');
+      body = body.concat('البريد الإلكتروني: ').concat(this.state.formFields.email.value).concat(' \n ');
+      body = body.concat(this.state.formFields.body.value);
+      Linking.openURL(
+        // AnasCenterAT@Gmail.com
+        "mailto:nermeenmattar5d@gmail.com?subject="
+          .concat(this.state.formFields.subject.value)
+          .concat("&body=")
+          .concat(body)
+      ).then(res => {
+        Alert.alert('تم الارسال بنجاح');
+        this.clearAllFields();
+      }).catch(err => {
+        Alert.alert( 'فشل الإرسال', 'الرجاء المحاولة مرةً أخرى');
+      }); 
+    }
   }
 
-  onTextChanged(text, fieldIndex) {
+  clearAllFields() {
     const formFields = this.state.formFields;
-    formFields[fieldIndex].value = text;
+    Object.keys(formFields).forEach(fieldName => {
+      formFields[fieldName].value = '';
+    });
+    this.setState({formFields: formFields});
+  }
+
+  displayAlertMessage() {
+    Alert.alert('فشل الإرسال', "الرجاء تعبئة جميع الخانات");
+  }
+
+  onTextChanged(text, fieldName) {
+    const formFields = this.state.formFields;
+    formFields[fieldName].value = text;
     this.setState({
-        formFields: formFields
+      formFields: formFields
     });
   }
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -117,26 +164,25 @@ const styles = StyleSheet.create({
   textInput: {
     height: 41,
     backgroundColor: Colors.primary,
-    fontFamily: 'Tajawal',
+    fontFamily: "Tajawal",
     fontSize: 17,
-    textAlign: 'right',
+    textAlign: "right",
     padding: 8,
-    paddingTop: 12, // 20 didn't work
+    paddingTop: 12 // 20 didn't work
   },
   button: {
     backgroundColor: Colors.brand,
-    padding:10,
+    padding: 10
   },
   buttonText: {
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
     fontSize: 17
   },
   inputStyle: {
     width: 315,
-    minWidth: '60%',
+    minWidth: "60%",
     marginTop: 11,
     borderRadius: 10 /* **N** */
   }
-
 });
