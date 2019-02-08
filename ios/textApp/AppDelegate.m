e5518af10938e5e4d97c7d2be191c4a71eeeb7d0
nermeen-mattar <nermeenmattar5d@gmail.com>
2019-02-08 20:16:08 +0200

    Add new sounds

--- ios/textApp/AppDelegate.m
+++ ios/textApp/AppDelegate.m
@@ -9,7 +9,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-#import "RNSplashScreen.h"  // here
+// #import "RNSplashScreen.h"  // here
 
 @implementation AppDelegate
 
@@ -30,7 +30,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  [RNSplashScreen show];
+  // [RNSplashScreen show];
   return YES;
 }
 
