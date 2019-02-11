0be1e3b217ae7f839360dc40ab635589ac292f93
nermeen-mattar <nermeenmattar5d@gmail.com>
2019-02-11 14:59:12 +0200

    Improve performance in speaking screen by calling text predection only when the user is done typing.

--- src/screens/SpeakingScreen.js
+++ src/screens/SpeakingScreen.js
@@ -1,62 +1,68 @@
-import React from 'react';
-import { ScrollView, StyleSheet, TextInput, View, TouchableOpacity, TouchableHighlight, Image, Keyboard} from 'react-native';
-// import { ExpoLinksView } from '@expo/samples';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React from "react";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  Keyboard,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-import Colors from '../constants/Colors';
-import CustomHeader from '../components/CustomHeader';
-import { TextToSpeach } from '../classes/TextToSpeach';
-import { MonoText } from '../components/StyledText';
-import { Storage } from '../classes/Storage';
-import { TextPrediction } from '../classes/TextPrediction';
-import commonStyles from '../styles/commonStyles';
-import ConfirmDeleteDialog from '../components/ConfirmDeleteDialog';
+import Colors from "../constants/Colors";
+import CustomHeader from "../components/CustomHeader";
+import { TextToSpeach } from "../classes/TextToSpeach";
+import { MonoText } from "../components/StyledText";
+import { Storage } from "../classes/Storage";
+import { TextPrediction } from "../classes/TextPrediction";
+import commonStyles from "../styles/commonStyles";
+import ConfirmDeleteDialog from "../components/ConfirmDeleteDialog";
 
 export default class TextToSpeachScreen extends React.Component {
   constructor(props) {
     super();
     this.state = {
       inputPlaceholder: "أكتب",
-      text: '',
-      predectedWords: TextPrediction.getInstance().getPredectedWords(''),
+      text: "",
+      predectedWords: TextPrediction.getInstance().getPredectedWords(""),
       toolsBar: [
         {
-          title: 'نطق',
-          iconName: 'volume-up',
+          title: "نطق",
+          iconName: "volume-up",
           iconSize: 34,
           onPress: () => this.speak(),
-           styles: [styles.tool]
+          styles: [styles.tool]
         },
         {
-          title: 'مسافة',
+          title: "مسافة",
           customIcon: styles.spaceIcon,
           onPress: () => this.addSpace(),
           // style={styles.space}
           styles: [styles.space]
         },
         {
-          title: 'المفضلة',
-          iconName: 'star',
+          title: "المفضلة",
+          iconName: "star",
           onPress: () => this.toggleFavourites(),
           styles: [styles.tool]
         },
         {
-          title: 'مسح',
-          iconName: 'trash',
+          title: "مسح",
+          iconName: "trash",
           onPress: () => this.clear(),
           styles: [styles.tool]
         }
       ],
       toolsColors: {
-        'نطق': Colors.brand,
-        'المفضلة': Colors.borderColor,
-        'مسح': Colors.borderColor,
-        'مسافة': Colors.borderColor
+        نطق: Colors.brand,
+        المفضلة: Colors.borderColor,
+        مسح: Colors.borderColor,
+        مسافة: Colors.borderColor
       },
-      activeToolName: '' // 'نطق',
+      activeToolName: "" // 'نطق',
     };
     this.initFavourites();
-    props.navigation.addListener('willFocus', this.load)
+    props.navigation.addListener("willFocus", this.load);
   }
 
   load = () => {
@@ -65,128 +71,159 @@ export default class TextToSpeachScreen extends React.Component {
       // activeToolName: 'نطق',
       // text: JSON.stringify(this.props.navigation.getParam('prevRoute') !== AlertScreen.name),
       toolsColors: {
-        'نطق': Colors.brand,
-        'المفضلة': Colors.borderColor,
-        'مسح': Colors.borderColor,
-        'مسافة': Colors.borderColor
-      },
+        نطق: Colors.brand,
+        المفضلة: Colors.borderColor,
+        مسح: Colors.borderColor,
+        مسافة: Colors.borderColor
+      }
     });
     // if(this.props.navigation.getParam('prevRoute') !== AlertScreen.name) {
     //   this.onTextChanged('');
-    // } 
- }
+    // }
+  };
 
   componentDidMount() {
-    TextPrediction.getInstance().initIndexedDefaultWords() 
+    TextPrediction.getInstance().initIndexedDefaultWords();
   }
- 
+
   static navigationOptions = {
     header: null
   };
 
-
   render() {
     return (
       <TouchableOpacity
-      style={styles.container}
-      activeOpacity = {1}
-      onPress={Keyboard.dismiss} 
-    >
-        <CustomHeader navigation = {this.props.navigation} />
+        style={styles.container}
+        activeOpacity={1}
+        onPress={Keyboard.dismiss}
+      >
+        <CustomHeader navigation={this.props.navigation} />
 
-      <View style={commonStyles.flexCenter}>
+        <View style={commonStyles.flexCenter}>
+          <View style={styles.inputAndToolsWrapper}>
+            <TextInput
+              value={this.state.text}
+              style={styles.textInput}
+              onChangeText={text => {this.setState({text: text}); this.onTextChanged(text)}}
+              placeholder={this.state.inputPlaceholder}
+              multiline={true}
+            />
 
+            <View style={styles.toolsbar}>
+              {this.state.toolsBar.map(tool => {
+                return (
+                  <TouchableHighlight
+                    style={tool.styles}
+                    underlayColor="transparent"
+                    onPress={tool.onPress}
+                    onShowUnderlay={() =>
+                      this.setState({ activeToolName: tool.title })
+                    }
+                    onHideUnderlay={() => this.setState({ activeToolName: "" })}
+                  >
+                    <View>
+                      {tool.customIcon ? (
+                        <MonoText
+                          style={[
+                            tool.customIcon,
+                            {
+                              backgroundColor: this.state.toolsColors[
+                                tool.title
+                              ]
+                            }
+                          ]}
+                        >
+                                                 </MonoText>
+                      ) : (
+                        // this.state.activeToolName === 'مسافة' ?  Colors.brand : Colors.borderColor}
 
-      <View style={styles.inputAndToolsWrapper}> 
-        <TextInput value={this.state.text} style={styles.textInput} onChangeText={(text) => this.onTextChanged(text)}
-        placeholder= {this.state.inputPlaceholder}  multiline = {true} />
-     
-     
-        <View style={styles.toolsbar}> 
-
-        {
-            this.state.toolsBar.map(tool => {
-              return(
-            <TouchableHighlight style={tool.styles}
-            underlayColor = "transparent"
-            onPress={tool.onPress} 
-            onShowUnderlay={()=>this.setState({activeToolName:tool.title})}
-            onHideUnderlay={()=>this.setState({activeToolName:''})}>    
-          <View> 
-          {
-            tool.customIcon ?
-            <MonoText     
-            style={[tool.customIcon, 
-              {backgroundColor:  this.state.toolsColors[tool.title]}]} > </MonoText>
-              // this.state.activeToolName === 'مسافة' ?  Colors.brand : Colors.borderColor}
-        
-            : <Icon style={{textAlign: 'center'}}  name={tool.iconName} size={tool.iconSize || 24} color={this.state.toolsColors[tool.title]}/> 
-            // color={ this.state.activeToolName === tool.title ? Colors.brand: Colors.borderColor}
-          }
-            <MonoText style={{ textAlign: 'center',  color: this.state.toolsColors[tool.title]}}> {tool.title} </MonoText>
-            {/* color: this.state.activeToolName === tool.title ? Colors.brand: Colors.borderColor */}
+                        <Icon
+                          style={{ textAlign: "center" }}
+                          name={tool.iconName}
+                          size={tool.iconSize || 24}
+                          color={this.state.toolsColors[tool.title]}
+                        />
+                      )
+                      // color={ this.state.activeToolName === tool.title ? Colors.brand: Colors.borderColor}
+                      }
+                      <MonoText
+                        style={{
+                          textAlign: "center",
+                          color: this.state.toolsColors[tool.title]
+                        }}
+                      >
+                                               {tool.title}{" "}
+                      </MonoText>
+                      {/* color: this.state.activeToolName === tool.title ? Colors.brand: Colors.borderColor */}
+                    </View>
+                  </TouchableHighlight>
+                );
+              })}
+            </View>
           </View>
-        </TouchableHighlight>
-              )})
-            }
         </View>
-        </View> 
-
-        </View>
-
-  
 
         <View style={styles.predectionsWrapper}>
-        {
-            this.state.predectedWords.map(word => {
-              return(
-        <TouchableOpacity  style={[styles.predectedWord, commonStyles.shadow ]} onPress={() =>
-         this.selectPredectedWord(word)}>
-            <MonoText style={styles.predectedWordText}> {word} </MonoText>
-    </TouchableOpacity>
-              )})
-              }
-          </View>
+          {this.state.predectedWords.map(word => {
+            return (
+              <TouchableOpacity
+                style={[styles.predectedWord, commonStyles.shadow]}
+                onPress={() => this.selectPredectedWord(word)}
+              >
+                <MonoText style={styles.predectedWordText}> {word} </MonoText>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
 
-        {
-         this.state.showConfirmDialog ? <ConfirmDeleteDialog
-        customizedText = {"هل أنت متأكد أنك تربد حذف" 
-        + " '" +  this.state.text + " '" + 
-        "من المفضلات"}  
-         onConfirm={() => {
-          this.setState({
-            showConfirmDialog: false
-          });
-           this.deleteFavourite();
-         }}
-         onCancel={() => {
-          this.setState({
-            showConfirmDialog: false
-          });
-        }}> </ConfirmDeleteDialog> : null
-       }
+        {this.state.showConfirmDialog ? (
+          <ConfirmDeleteDialog
+            customizedText={
+              "هل أنت متأكد أنك تربد حذف" +
+              " '" +
+              this.state.text +
+              " '" +
+              "من المفضلات"
+            }
+            onConfirm={() => {
+              this.setState({
+                showConfirmDialog: false
+              });
+              this.deleteFavourite();
+            }}
+            onCancel={() => {
+              this.setState({
+                showConfirmDialog: false
+              });
+            }}
+          >
+                     </ConfirmDeleteDialog>
+        ) : null}
       </TouchableOpacity>
     );
   }
 
-
   selectPredectedWord(word) {
     let inputText = this.state.text;
     const indexOfLastWord = inputText.lastIndexOf(" ");
-    if(indexOfLastWord === -1) {  // writing the first word
-      inputText = '';
+    if (indexOfLastWord === -1) {
+      // writing the first word
+      inputText = "";
     }
     if (indexOfLastWord !== inputText.length - 1) {
-      inputText = inputText.slice(0, indexOfLastWord).concat(' ');
+      inputText = inputText.slice(0, indexOfLastWord).concat(" ");
     }
-    this.onTextChanged(inputText.concat(word).concat(' '))
+    this.changeText(inputText.concat(word).concat(" "));
   }
-  
-  onTextChanged(text) {
+
+
+  changeText(text) {
+    this.setState({text: text});
     const toolsColors = this.state.toolsColors;
-    toolsColors['المفضلة'] = this.state.favourites.includes(text) ? Colors.brand : Colors.borderColor;
+    toolsColors["المفضلة"] = this.state.favourites.includes(text)
+      ? Colors.brand
+      : Colors.borderColor;
     this.setState({
-      text: text,
       toolsColors: toolsColors,
       predectedWords: TextPrediction.getInstance().getPredectedWords(text) // .slice(0, 12)
     });
@@ -195,164 +232,178 @@ export default class TextToSpeachScreen extends React.Component {
   speak() {
     let toolsBar = [...this.state.toolsBar];
     toolsBar[0].styles = [styles.tool, styles.speakActive];
-    this.setState({toolsBar: toolsBar});
-    TextPrediction.getInstance().addToUserWordsIfNew(this.state.text); 
+    this.setState({ toolsBar: toolsBar });
+    TextPrediction.getInstance().addToUserWordsIfNew(this.state.text);
     TextToSpeach.getInstance().speak(this.state.text);
     setTimeout(() => {
       toolsBar[0].styles = [styles.tool];
-      this.setState({toolsBar: toolsBar});
-
-    }, this.state.text.length/10.5 * 1000);
+      this.setState({ toolsBar: toolsBar });
+    }, (this.state.text.length / 10.5) * 1000);
   }
 
   addSpace() {
     this.setState({
-      text: this.state.text.concat(' '),
+      text: this.state.text.concat(" ")
     });
   }
 
   clear() {
-    this.onTextChanged('')
+    this.changeText("");
   }
 
   initFavourites() {
     const storageInstance = Storage.getInstance();
-    const result = {value: null};
-    storageInstance.getItem('favourites', result).then(res => {
-      const currFavourites = result.value ? result.value : []; 
+    const result = { value: null };
+    storageInstance.getItem("favourites", result).then(res => {
+      const currFavourites = result.value ? result.value : [];
       this.setState({
         favourites: currFavourites
       });
     });
   }
 
-
-
   deleteFavourite() {
     let currFavourites = this.state.favourites;
     const storageInstance = Storage.getInstance();
     const toolsColors = this.state.toolsColors;
-    toolsColors['المفضلة'] = Colors.borderColor;
-    if(currFavourites.includes(this.state.text)) {
-      currFavourites = currFavourites.filter(favourite => favourite !== this.state.text);
-      storageInstance.setItem('favourites', currFavourites).then(res => {
-      }); 
+    toolsColors["المفضلة"] = Colors.borderColor;
+    if (currFavourites.includes(this.state.text)) {
+      currFavourites = currFavourites.filter(
+        favourite => favourite !== this.state.text
+      );
+      storageInstance.setItem("favourites", currFavourites).then(res => {});
     }
 
     this.setState({
       favourites: currFavourites,
       toolsColors: toolsColors
-    }); 
+    });
   }
 
   toggleFavourites() {
-    if(!this.state.text.trim().length) {
+    if (!this.state.text.trim().length) {
       return;
     }
     const storageInstance = Storage.getInstance();
     const toolsColors = this.state.toolsColors;
     let currFavourites = this.state.favourites;
-    if(toolsColors['المفضلة']  == Colors.brand) {
-      this.setState({showConfirmDialog: true});   
+    if (toolsColors["المفضلة"] == Colors.brand) {
+      this.setState({ showConfirmDialog: true });
     } else {
-      toolsColors['المفضلة'] = Colors.brand;
-      if(!currFavourites.includes(this.state.text)) {
-        currFavourites = [...currFavourites,  this.state.text];
-        storageInstance.setItem('favourites', currFavourites).then(res => {
-        });
+      toolsColors["المفضلة"] = Colors.brand;
+      if (!currFavourites.includes(this.state.text)) {
+        currFavourites = [...currFavourites, this.state.text];
+        storageInstance.setItem("favourites", currFavourites).then(res => {});
       }
     }
     this.setState({
       favourites: currFavourites,
       toolsColors: toolsColors
-    }); 
+    });
   }
 
+  onTextChanged = this.debounce((text) => {
+    this.changeText(text);
+   }, 300);
+
+  debounce(a, b, c) {
+    let d, e;
+    return function() {
+      function h() {
+        (d = null), c || (e = a.apply(f, g));
+      }
+      let f = this,
+        g = arguments;
+      return (
+        clearTimeout(d),
+        (d = setTimeout(h, b)),
+        c && !d && (e = a.apply(f, g)),
+        e
+      );
+    };
+  }
 }
 const styles = StyleSheet.create({
-container: {
-  flex: 1
-  //     padding: 13,
-},
-inputAndToolsWrapper: {
-  marginTop: 10,
-  paddingTop: 12,
-  height: 144,
-  width: 350,
-  position: 'relative',
-  // marginHorizontal: 'auto',
-  backgroundColor: Colors.primary,
-  borderRadius: 10 /* **N** */
-},
-textInput: {
-  // width: 350,
-  height:  60,
-  // backgroundColor: Colors.primary,
-  fontSize: 21,
-  //   color: Colors.textSecondary,
-  textAlign: 'right',
-  paddingHorizontal: 8, // 11
-  // paddingVertical: 12, // 16 // moved to wrapper
-  // marginLeft: 'auto', // 10, // 12
-  // marginRight: 'auto',
-},
-toolsbar: {
-position: 'absolute',
-top: 84,
-backgroundColor: '#F7F7F7', // temp
-flexDirection: 'row',
-justifyContent: 'center',
-// marginLeft: 'auto',
-// marginRight: 'auto',
-// marginLeft: 'auto', // 10, // 12
-// marginRight: 'auto',
-height: 60,
-borderRadius: 10 /* **N** */
-
-},
-tool: {
-  paddingVertical: 5,
-  // width: 60,
-  paddingHorizontal: 16,
-  textAlign: 'center'
-},
-speakActive: {
-  backgroundColor: Colors.orange, // f9812A
-  borderRadius: 10 
-},
-space: {
-  paddingVertical: 10,
-  paddingHorizontal: 16,
-  textAlign: 'center',
-},
-spaceIcon: {
-  width: 116,
-  height: 22,
-  backgroundColor:  Colors.borderColor,
-  borderRadius: 10,
-  marginBottom: 4
-}, 
-predectionsWrapper: {
-  flexDirection: 'row',
-  justifyContent: 'center',
-  flexWrap: 'wrap',
-  paddingVertical: 14, 
-  paddingHorizontal: 12  
-}, 
-predectedWord: {
-  marginHorizontal: 10,
-  backgroundColor: 'white',
-  borderRadius: 10,
-  // paddingHorizontal: 24,
-  paddingVertical: 6, // was 10
-  marginBottom: 10,
-  // commented paddingHorizantl and added static width instead
-  width: 95,
-
-},
-predectedWordText: {
-  fontSize: 18,
-  textAlign: 'center'
-}
+  container: {
+    flex: 1
+    //     padding: 13,
+  },
+  inputAndToolsWrapper: {
+    marginTop: 10,
+    paddingTop: 12,
+    height: 144,
+    width: 350,
+    position: "relative",
+    // marginHorizontal: 'auto',
+    backgroundColor: Colors.primary,
+    borderRadius: 10 /* **N** */
+  },
+  textInput: {
+    // width: 350,
+    height: 60,
+    // backgroundColor: Colors.primary,
+    fontSize: 21,
+    //   color: Colors.textSecondary,
+    textAlign: "right",
+    paddingHorizontal: 8 // 11
+    // paddingVertical: 12, // 16 // moved to wrapper
+    // marginLeft: 'auto', // 10, // 12
+    // marginRight: 'auto',
+  },
+  toolsbar: {
+    position: "absolute",
+    top: 84,
+    backgroundColor: "#F7F7F7", // temp
+    flexDirection: "row",
+    justifyContent: "center",
+    // marginLeft: 'auto',
+    // marginRight: 'auto',
+    // marginLeft: 'auto', // 10, // 12
+    // marginRight: 'auto',
+    height: 60,
+    borderRadius: 10 /* **N** */
+  },
+  tool: {
+    paddingVertical: 5,
+    // width: 60,
+    paddingHorizontal: 16,
+    textAlign: "center"
+  },
+  speakActive: {
+    backgroundColor: Colors.orange, // f9812A
+    borderRadius: 10
+  },
+  space: {
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    textAlign: "center"
+  },
+  spaceIcon: {
+    width: 116,
+    height: 22,
+    backgroundColor: Colors.borderColor,
+    borderRadius: 10,
+    marginBottom: 4
+  },
+  predectionsWrapper: {
+    flexDirection: "row",
+    justifyContent: "center",
+    flexWrap: "wrap",
+    paddingVertical: 14,
+    paddingHorizontal: 12
+  },
+  predectedWord: {
+    marginHorizontal: 10,
+    backgroundColor: "white",
+    borderRadius: 10,
+    // paddingHorizontal: 24,
+    paddingVertical: 6, // was 10
+    marginBottom: 10,
+    // commented paddingHorizantl and added static width instead
+    width: 95
+  },
+  predectedWordText: {
+    fontSize: 18,
+    textAlign: "center"
+  }
 });
-
