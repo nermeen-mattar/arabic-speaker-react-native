409034bd549c98c89644a2732d277395870f9ac1
Nermeen Mattar <n.mattar@posrocket.com>
2019-04-28 20:18:50 +0300

    Adding fetch blob

--- android/app/src/main/java/com/textapp/MainApplication.java
+++ android/app/src/main/java/com/textapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.textapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import net.no_mad.tts.TextToSpeechPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new SplashScreenReactPackage(),
             new VectorIconsPackage(),
             new TextToSpeechPackage(),
