abf58a1c3359e73b8c8ed826cfb96a83d926d522
Nermeen Mattar <v-nmattar@expedia.com>
2020-05-20 06:57:01 +0300

    upgrade to 59.10

---
+++ metro.config.js
@@ -0,0 +1,12 @@
+// metro.config.js`
+
+module.exports = {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+    },
+  }
